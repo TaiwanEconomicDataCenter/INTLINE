4fd8734a96f08682fa038428e6e6c81c38e8e47b
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-03-28 18:13:39 +0800

    Update INTLINE_extention.py

--- INTLINE_extention.py
+++ INTLINE_extention.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # pylint: disable=E1101
-#pip install regex, datetime, pandas, requests, openpyxl(3.0.3), lxml, xlrd(2.0.1), iteration_utilities, matplotlib, statsmodels, pathlib, bs4, selenium, webdriver_manager, quandl, pywin32, pycnnum, roman, html5lib, pyxlsb, dateparser, sqlalchemy, pymysql
+#pip install regex, datetime, pandas, requests, openpyxl(3.0.3), lxml, xlrd(2.0.1), xlwt, iteration_utilities, matplotlib, statsmodels, pathlib, bs4, selenium, webdriver_manager, quandl, pywin32, pycnnum, roman, html5lib, pyxlsb, dateparser, sqlalchemy, pymysql
 import math, sys, calendar, os, copy, time, shutil, logging, ssl, zipfile, traceback, pycnnum, roman, dateparser
 import regex as re
 import pandas as pd
